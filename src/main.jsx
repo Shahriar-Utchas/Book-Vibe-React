d7b453b0741cdb4b701c872d58d470175eda08e2
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2025-04-18 10:17:54 +0600

    Navbar Added

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World! pmk</div>,
-  },
-]);
+import { router } from './Routes/Routes';
+
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
